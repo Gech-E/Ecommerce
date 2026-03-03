a50ce86414a3506474273dd8149004e2721e30c0
Gech-E <getachewekubay8@gmail.com>
2026-03-03 20:07:51 +0300

    move he homepage in to react

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
-
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    )
 }
 
 export default App
